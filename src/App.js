efdb771e03222b15449a6f35ee8432454895c4d0
surepos <125876053+surepos@users.noreply.github.com>
2024-07-18 01:23:48 +0900

    gf

--- src/App.js
+++ src/App.js
@@ -55,7 +55,7 @@ function App() {
    useEffect(() => {
        const fetchTasks = async () => {
            try {
-               const response = await axios.get(`http://localhost:8080/tasks/${displayDate.toISOString().split('T')[0]}`);
+               const response = await axios.get(`/tasks/${displayDate.toISOString().split('T')[0]}`);
                setTasks(response.data);
                initializeTaskDoneState(response.data); 
            } catch (error) {
