6fce6e5ae8e2acfd1ffc247800e258cb837230d5
surepos <125876053+surepos@users.noreply.github.com>
2024-07-18 00:48:08 +0900

    proxy2

--- src/App.js
+++ src/App.js
@@ -55,7 +55,7 @@ function App() {
    useEffect(() => {
        const fetchTasks = async () => {
            try {
-               const response = await axios.get(`http://localhost:8080/tasks/${displayDate.toISOString().split('T')[0]}`);
+               const response = await axios.get(`https://3402-211-186-131-234.ngrok-free.app/tasks/${displayDate.toISOString().split('T')[0]}`);
                setTasks(response.data);
                initializeTaskDoneState(response.data); 
            } catch (error) {
@@ -85,7 +85,7 @@ function App() {
 
    const toggleTaskDone = async (taskId) => {
        try {
-           const response = await axios.put(`https://3402-211-186-131-234.ngrok-free.app/task/${taskId}`);
+           const response = await axios.put(`http://localhost:8080/task/${taskId}`);
            const updatedTask = response.data;
 
            setTaskDone({
